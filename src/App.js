7016c9481c6ec9dffa6653a8970694109d1bd03c
Nahue <nahue.santini@gmail.com>
2023-09-26 17:38:02 -0300

    feature(route to Home in app.js): Added route to home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div className="App">
       <h1>Proyecto Final</h1>
+
+      <Routes>
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </div>
   );
 }
