b42868fa52df671a7c3795fdc9e8115fc1033d19
Nahue <nahue.santini@gmail.com>
2023-09-28 16:00:06 -0300

    deleted(App.js) Deleted in App.js "<h1> Proyecto Final </h1>"

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
   return (
     <div className="App">
 
-      <h1>Proyecto Final</h1>
       <Routes>
         <Route path='/login' element={<Login/>}/>
         <Route path='/register' element={<Register/>}/> 
