27e6fe1cba7c1e9c0dc7f1a62b8a2a0080142ed4
Franco Adamoli <franco.adamoli@gmail.com>
2023-09-28 01:33:33 -0300

    Install TailwindCSS

--- src/components/Cart/cart.jsx
+++ src/components/Cart/cart.jsx
@@ -18,8 +18,11 @@ const CartComponent = () => {
 
   return (
     <div>
-      <Nav/>
-      {/* <p>{models}</p> */}
+      <Nav />
+      <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+          <div class="text-xl font-medium text-black">ChitChat</div>
+          <p class="text-slate-500">You have a new message!</p>
+      </div>
     </div>
   );
 };
