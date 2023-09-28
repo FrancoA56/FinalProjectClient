f70bd16e23ee09e5ce888c60123ac11e3ee752c1
Nahue <nahue.santini@gmail.com>
2023-09-28 16:38:55 -0300

    fix (components/Home/HomeComponent.jsx) fix div container

--- src/components/Home/HomeComponent.jsx
+++ src/components/Home/HomeComponent.jsx
@@ -1,6 +1,7 @@
 import style from "./home.module.css";
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import plantillas from "../../img/img";
 
 const HomeComponent = () => {
   const [activeLeftCard, setActiveLeftCard] = useState(false);
@@ -23,7 +24,7 @@ const HomeComponent = () => {
   };
 
   return (
-    <>
+    <div className={style.cont}>
       <div className={style.container}>
         <div
           onClick={handleImageLeftClick}
@@ -31,8 +32,8 @@ const HomeComponent = () => {
         >
           <img
             className={style.img}
-            src="https://img.freepik.com/foto-gratis/espacio-copia-plantilla-interfaz-usuario-diseno-pagina-web_53876-120721.jpg?w=740&t=st=1695764275~exp=1695764875~hmac=561c2f2a7521b627fbc0f53c66876e0a442a048ccd66580a099877d591c9faa7"
-            alt=""
+            src={plantillas[0].url}
+            alt={plantillas[0].name}
           />
         </div>
 
@@ -46,8 +47,8 @@ const HomeComponent = () => {
         >
           <img
             className={style.img}
-            src="https://img.freepik.com/foto-gratis/diseno-sitio-web-diseno-contenido-grafico_53876-21203.jpg?w=900&t=st=1695764493~exp=1695765093~hmac=cdb7a794709568c68dd9e920d2dd61de0b817666c2004b75512a2de9accaa44b"
-            alt=""
+            src={plantillas[1].url}
+            alt={plantillas[1].name}
           />
         </div>
 
@@ -57,8 +58,8 @@ const HomeComponent = () => {
         >
           <img
             className={style.img}
-            src="https://img.freepik.com/foto-gratis/crear-banner-blanco-diseno-sitio-web_53876-132284.jpg?w=996&t=st=1695764391~exp=1695764991~hmac=586b0fc2239f7edbc8d387c7eff11afbbe9616ed0d61764e704f72a45da9d98f"
-            alt=""
+            src={plantillas[2].url}
+            alt={plantillas[2].name}
           />
         </div>
       </div>
@@ -67,7 +68,7 @@ const HomeComponent = () => {
           <button className={style.buttonShop}>Shop plantilla</button>
         </NavLink>
       </div>
-    </>
+    </div>
   );
 };
 
