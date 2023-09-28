10802cefa7d8fc8d5fbaad29a9361a30507a0a4d
Corto07 <claudiocarruz@gmail.com>
2023-09-28 14:26:04 -0300

    Add ruotes to App - Validate login and register

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import React from "react";
+import {Route, Routes} from "react-router-dom";
+import Login from './views/login';
+import Register from './views/register';
 
 function App() {
   return (
     <div className="App">
       <h1>Proyecto Final</h1>
+      <Routes>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/> 
+      </Routes> 
     </div>
   );
 }
