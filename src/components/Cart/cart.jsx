2a3f5b26acde5eaeacf072a23dddd48d257e94ac
Franco Adamoli <franco.adamoli@gmail.com>
2023-09-28 01:39:48 -0300

    Structure Cart.jsx in components folder

--- src/components/Cart/cart.jsx
+++ src/components/Cart/cart.jsx
@@ -17,13 +17,92 @@ const CartComponent = () => {
   // }, models);
 
   return (
-    <div>
+    <>
       <Nav />
-      <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-          <div class="text-xl font-medium text-black">ChitChat</div>
-          <p class="text-slate-500">You have a new message!</p>
+      <div className="bg-gray-100 p-4">
+        <h1 className="text-2xl font-bold mb-4">Carrito de Compras</h1>
+        {/* Aquí puedes listar los productos en el carrito */}
+        <div className="bg-white p-4 rounded shadow-md">
+          {/* Detalles del producto */}
+          <div className="flex justify-between items-center mb-2">
+            <img
+              src="imagen-producto.jpg"
+              alt="Producto"
+              className="w-16 h-16"
+            />
+            <div className="flex-1 ml-4">
+              <h2 className="text-lg font-semibold">Nombre del Producto</h2>
+              <p className="text-gray-600">Precio: $XX.XX</p>
+            </div>
+            <div className="flex items-center">
+              <span className="text-gray-600">Cantidad:</span>
+              <input
+                type="number"
+                className="w-10 h-8 ml-2 border border-gray-300 rounded"
+              />
+            </div>
+          </div>
+          {/* Más productos pueden ir aquí */}
+        </div>
+        <div className="bg-white p-4 rounded shadow-md">
+          {/* Detalles del producto */}
+          <div className="flex justify-between items-center mb-2">
+            <img
+              src="imagen-producto.jpg"
+              alt="Producto"
+              className="w-16 h-16"
+            />
+            <div className="flex-1 ml-4">
+              <h2 className="text-lg font-semibold">Nombre del Producto</h2>
+              <p className="text-gray-600">Precio: $XX.XX</p>
+            </div>
+            <div className="flex items-center">
+              <span className="text-gray-600">Cantidad:</span>
+              <input
+                type="number"
+                className="w-10 h-8 ml-2 border border-gray-300 rounded"
+              />
+            </div>
+          </div>
+          {/* Más productos pueden ir aquí */}
+        </div> <div className="bg-white p-4 rounded shadow-md">
+          {/* Detalles del producto */}
+          <div className="flex justify-between items-center mb-2">
+            <img
+              src="imagen-producto.jpg"
+              alt="Producto"
+              className="w-16 h-16"
+            />
+            <div className="flex-1 ml-4">
+              <h2 className="text-lg font-semibold">Nombre del Producto</h2>
+              <p className="text-gray-600">Precio: $XX.XX</p>
+            </div>
+            <div className="flex items-center">
+              <span className="text-gray-600">Cantidad:</span>
+              <input
+                type="number"
+                className="w-10 h-8 ml-2 border border-gray-300 rounded"
+              />
+            </div>
+          </div>
+          {/* Más productos pueden ir aquí */}
+        </div>
+        {/* Total y botones de acción */}
+        <div className="mt-4">
+          <div className="flex justify-between items-center">
+            <p className="text-xl font-semibold">Total: $XXX.XX</p>
+            <div>
+              <button className="bg-blue-500 text-white px-4 py-2 rounded mr-2">
+                Pagar
+              </button>
+              <button className="bg-red-500 text-white px-4 py-2 rounded">
+                Vaciar Carrito
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
