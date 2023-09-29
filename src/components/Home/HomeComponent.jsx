9f0ed0c0fe350aedeae37545a8c118b332bba388
Nahue <nahue.santini@gmail.com>
2023-09-29 20:08:41 -0300

    Add(components/Home.jsx) Add Navlink to "/detail/id" in Card image & and Get image Url from global state

--- src/components/Home/HomeComponent.jsx
+++ src/components/Home/HomeComponent.jsx
@@ -2,8 +2,12 @@ import style from "./home.module.css";
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import plantillas from "../../utils/img/exampleModel.json";
+import { useSelector } from "react-redux";
 
 const HomeComponent = () => {
+  //! Traigo las plantillas de las mejores rankeadas del estado Global (aun no se como se llaman)
+  //! const bestRanked = useSelector((state) => state.bestRanked)
+
   // Estados locales para hacer la "animacion"
   const [activeLeftCard, setActiveLeftCard] = useState(false);
   const [activeRigthCard, setActiveRigthCard] = useState(false);
@@ -33,12 +37,15 @@ const HomeComponent = () => {
         <div
           onClick={handleImageLeftClick}
           className={!activeLeftCard ? style.cardLeft : style.cardActiveL}
-        >
+          >
+          {/* QUEDA ARMADO PARA QUE LA IMAGEN TE LLEVE AL DETAIL DE LA PLANTILLA (si es que hay) */}
+          {/* <NavLink to={`/detail/${bestRanked[1].id}`}> */}
           <img
             className={style.img}
-            src={plantillas[0].url}
-            alt={plantillas[0].name}
+            src={plantillas[0].url} /* src={bestRanked[1].url} */
+            alt={plantillas[0].name} /* alt={bestRanked[1].name} */
           />
+          {/* </NavLink> */}
         </div>
 
         <div
@@ -49,22 +56,28 @@ const HomeComponent = () => {
             style.cardCenter
           }
         >
-          <img
-            className={style.img}
-            src={plantillas[1].url}
-            alt={plantillas[1].name}
-          />
+          {/* QUEDA ARMADO PARA QUE LA IMAGEN TE LLEVE AL DETAIL DE LA PLANTILLA (si es que hay) */}
+          {/* <NavLink to={`/detail/${bestRanked[0].id}`}> */}
+            <img
+              className={style.img}
+              src={plantillas[1].url} /* src={bestRanked[0].url} */
+              alt={plantillas[1].name} /* alt={bestRanked[0].name} */
+            />
+          {/* </NavLink> */}
         </div>
 
         <div
           onClick={handleImageRigthClick}
           className={!activeRigthCard ? style.cardRigth : style.cardActiveR}
-        >
-          <img
-            className={style.img}
-            src={plantillas[7].url}
-            alt={plantillas[4].name}
-          />
+          >
+          {/* QUEDA ARMADO PARA QUE LA IMAGEN TE LLEVE AL DETAIL DE LA PLANTILLA (si es que hay) */}
+          {/* <NavLink to={`/detail/${bestRanked[2].id}`}> */}
+            <img
+              className={style.img}
+              src={plantillas[7].url} /* src={bestRanked[2].url} */
+              alt={plantillas[7].name} /* alt={bestRanked[2].name} */
+            />
+          {/* </NavLink> */}
         </div>
       </div>
       <div>
