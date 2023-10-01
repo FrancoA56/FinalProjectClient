49ce989ae2abb62ca7c365f18ff02433d2d381f9
Jime Mateo <jimemateo18@gmail.com>
2023-10-01 11:09:11 -0300

    Added Sign In and Profile to Nav Bar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -244,6 +244,17 @@ function Nav() {
                   Login
                 </a>
               </li>
+              
+              <li>
+                <a
+                  className="block w-full whitespace-nowrap bg-transparent px-3 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
+                  href="/register"
+                  data-te-dropdown-item-ref
+                >
+                  Sing In
+                </a>
+              </li>
+              
               <li>
                 <a
                   className="block w-full whitespace-nowrap bg-transparent px-3 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
