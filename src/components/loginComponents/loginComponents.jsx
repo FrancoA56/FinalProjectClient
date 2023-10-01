87bc09aeacaa1a2f9c0be5d8684f9cdb119fe269
Franco Adamoli <franco.adamoli@gmail.com>
2023-10-01 15:22:21 -0300

    login succesful now

--- src/components/loginComponents/loginComponents.jsx
+++ src/components/loginComponents/loginComponents.jsx
@@ -12,28 +12,6 @@ import axios from "axios";
  Para las validaciones del login comprobar que el mail existe y 
 comparar la contraseña ingresada con la cargada por el usuario */
 
-function Validation(input) {
-  let errors = {};
-
-  // Validate mail
-  if (!/^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/.test(input.email)) {
-    errors.email = "Incorrect email format";
-  }
-  if (!/^(?=.{1,35}$).+/.test(input.email)) {
-    errors.email = "Must be less than 35 characters";
-  } else {
-    errors.mail = "";
-  }
-  // Validate password
-  if (!/^(?=.*\d).{6,10}$/.test(input.password)) {
-    errors.password =
-      "l Password must have a number and between 6 to 10 characters";
-  } else {
-    errors.password = "";
-  }
-  return errors;
-}
-
 const LoginComponents = () => {
   const navigate = useNavigate();
   const [access, setAccess] = useState(false);
@@ -45,31 +23,15 @@ const LoginComponents = () => {
     password: "",
   });
 
-  const [errors, setErrors] = useState({
-    email: "",
-    password: "",
-  });
-
   function handleChange(e) {
     setInput({
       ...input,
       [e.target.name]: e.target.value,
     });
-    setErrors(
-      Validation({
-        ...input,
-        [e.target.name]: e.target.value,
-      })
-    );
   }
 
   async function handleSubmit(e) {
     e.preventDefault();
-    // const EMAIL = "claudiocarruz@gmail.com";
-    // const PASSWORD = "123456";
-
-    // Verificar si la información de usuario coincide
-    // if (users.email) {
     try {
       if (input.email && input.password) {
         const { data } = await axios.get(
@@ -82,9 +44,8 @@ const LoginComponents = () => {
         navigate("/");
       }
     } catch (error) {
-      console.log(error);
+      console.log("error:", error);
       alert("⛔ >>> email does not match password <<< ⛔");
-      // }
     }
   }
 
@@ -112,7 +73,6 @@ const LoginComponents = () => {
             required
           />
           <label htmlFor="email">Enter your email...</label>
-          <p style={{ color: "red" }}>{errors.email}</p>
         </div>
 
         <div>
@@ -126,7 +86,6 @@ const LoginComponents = () => {
             required
           />
           <label htmlFor="password">Enter password...</label>
-          <p style={{ color: "red" }}>{errors.password}</p>
         </div>
 
         <div>
