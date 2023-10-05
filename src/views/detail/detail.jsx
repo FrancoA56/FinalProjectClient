244de96a3cdc11537e81a53212cdbc4469d41fb1
Jime Mateo <jimemateo18@gmail.com>
2023-10-05 10:54:36 -0300

    Created Detail component and view. Added a new route to App.js

---
+++ src/views/detail/detail.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PresetDetail from "../../components/Detail/Detail";
+
+function Detail () {
+    return (
+        <div>
+            <PresetDetail/>
+        </div>
+    )
+}
+
+export default Detail;
