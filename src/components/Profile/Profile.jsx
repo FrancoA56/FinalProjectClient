4aefb65f7ac401aab72fe58dcd1305ab917b4ccb
Nahue <nahue.santini@gmail.com>
2023-10-18 18:32:57 -0300

    fix(profile.jsx) fix darkMode colors

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,8 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import Banner from "../Banner/Banner";
-import Nav from "../Nav/Nav";
 import { editUserRedux, logInUser } from "../../Redux/actions";
 import decodeToken from "../loginComponents/decodeToken";
 import Swal from "sweetalert2";
@@ -53,12 +51,17 @@ const Profile = () => {
   };
 
   // funcion edita la base de datos
- 
+
   const editUser = async (userEdit) => {
     try {
-      if(!userEdit.logo) {delete userEdit.logo}
+      if (!userEdit.logo) {
+        delete userEdit.logo;
+      }
       // Edita los datos del usuario y rcibe el token actualizado
-      const {data} = await axios.put(`${URL}/api/user/${user.email}`, userEdit); 
+      const { data } = await axios.put(
+        `${URL}/api/user/${user.email}`,
+        userEdit
+      );
       localStorage.setItem("token", data); // Almanecena el nuevo token en el localStorage
 
       const userDecode = decodeToken(data); // Decodifica el token
@@ -66,7 +69,7 @@ const Profile = () => {
       dispatch(editUserRedux(userDecode)); // Guarda los datos del usuario actualizado en el estado global
       showSuccessAlert("Your profile data has been updated");
     } catch (error) {
-      console.log(error.message)
+      console.log(error.message);
       showErrorAlert("Error");
     }
   };
@@ -77,9 +80,8 @@ const Profile = () => {
     // logInUser(userLocal)
   };
 
-
-   // --------------------------------------------------------------------------Alert-✅-----------
-   const showSuccessAlert = (message) => {
+  // --------------------------------------------------------------------------Alert-✅-----------
+  const showSuccessAlert = (message) => {
     Swal.fire({
       icon: "success",
       title: "Success",
@@ -98,22 +100,17 @@ const Profile = () => {
   };
   // --------------------------------------------------------------------------------⛔------------
 
-  
   return (
     <>
-      <div className="bg-gray-100 min-h-screen">
+      <div className="bg-gray-100 min-h-screen dark:bg-[#505050]">
         <div className="container mx-auto p-4">
           {/* Encabezado */}
-          <h1
-            className="inline-block mb-4 w-full rounded 5ec3bf px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal
-                     text-white shadow-[0_4px_9px_-4px_#000000] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.3),0_4px_18px_0_rgba(0,0,0,0.2)]"
-            style={{ "background-color": "#303030" }}
-          >
+          <h1 className="bg-[#303030] inline-block mb-4 w-full rounded 5ec3bf px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#000000]">
             Profile
           </h1>
           <div className="grid grid-cols-12 gap-4">
             {/* Columna izquierda */}
-            
+
             <div
               className="col-span-12 md:col-span-8 flex flex-col justify-start items-center rounded"
               style={{
@@ -123,14 +120,19 @@ const Profile = () => {
             >
               {/* //////////////////////////// */}
               <label className="block mt-3 text-xl text-white font-medium uppercase leading-normal">
-                   {user.name ? user.name : ""}
-                  </label>
+                {user.name ? user.name : ""}
+              </label>
               {/* Aca aparece el logo */}
               <div className="w-3/4 h-3/4 flex items-center justify-center">
                 <img
                   // className="max-w-full max-h-full rounded-md shadow"
                   className="h-72 object-cover rounded-md shadow"
-                  src={user.logo ? userLocal.logo || user.logo : userLocal.logo || "https://res.cloudinary.com/codecrafttemplates/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1696687386/no_imagen_am0gxq.jpg"}
+                  src={
+                    user.logo
+                      ? userLocal.logo || user.logo
+                      : userLocal.logo ||
+                        "https://res.cloudinary.com/codecrafttemplates/image/upload/v1697050849/codeCraft/logo_b_pstr1s.png"
+                  }
                   alt=""
                 />
               </div>
@@ -144,22 +146,16 @@ const Profile = () => {
             </div>
 
             {/* Columna derecha */}
-            <div
-              className="col-span-12 md:col-span-4 rounded"
-              style={{
-                background:
-                  "radial-gradient( 40rem circle at bottom, rgb(200, 200, 200), rgb(230, 230, 230)",
-              }}
-            >
+            <div className="col-span-12 md:col-span-4 rounded bg-gray-300 dark:bg-[#303030]">
               <div className="flex items-center">
                 <div>
-                  <div className="block m-3 text-sm font-medium uppercase leading-normal">
+                  <div className="block m-3 text-sm font-semibold uppercase leading-normal dark:text-[#707070]">
                     Edit Profile
                   </div>
                   <form>
                     <div className="mt-8 mb-9">
                       {/* //////////////////////////// */}
-      
+
                       {/* input del nombre*/}
                       <input
                         type="text"
@@ -168,7 +164,7 @@ const Profile = () => {
                         placeholder={user.name ? user.name : "DEFAULT"}
                         value={userLocal.name}
                         onChange={handleChange}
-                        className="shadow appearance-none border rounded-md w-3/4 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline first-letter:shadow-[0_4px_9px_-4px_#000000] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.3),0_4px_18px_0_rgba(0,0,0,0.2)]"
+                        className="shadow appearance-none border rounded-md w-3/4 py-2 px-3 text-gray-700 dark:bg-[#505050] dark:text-[#909090] leading-normal focus:outline-none focus:shadow-outline first-letter:shadow-[0_4px_9px_-4px_#000000] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.3),0_4px_18px_0_rgba(0,0,0,0.2)]"
                       />
                       {/* //////////////////////////// */}
 
@@ -182,9 +178,9 @@ const Profile = () => {
                         className="w-3/4 my-4 invisible absolute left-0"
                       />
                       <label
-                        title="Nahue"
+                        title="ButtonUpload"
                         for="file"
-                        class="inline-block mt-5 bg-[#909090] hover:bg-[#303030] w-3/4 rounded-md pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#000000] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.3),0_4px_18px_0_rgba(0,0,0,0.2)]"
+                        class="inline-block mt-5 bg-[#505050] hover:bg-[#303030] w-3/4 rounded-md pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#000000] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.3),0_4px_18px_0_rgba(0,0,0,0.2)]"
                       >
                         upload Image
                       </label>
@@ -200,9 +196,9 @@ const Profile = () => {
                         cols="40"
                         rows="5"
                         placeholder="..."
-                        className="indent-2 w-3/4 mt-5 rounded-md shadow-[0_4px_9px_-4px_#000000] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.3),0_4px_18px_0_rgba(0,0,0,0.2)]"
+                        className="indent-2 w-3/4 mt-5 rounded-md dark:bg-[#505050] dark:text-[#909090] leading-normal shadow-[0_4px_9px_-4px_#000000] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.3),0_4px_18px_0_rgba(0,0,0,0.2)]"
                       ></textarea>
-                      <p class="mt-1 text-sm leading-6 text-gray-600 mb-10">
+                      <p class="mt-1 text-sm leading-6 dark:text-[#909090] text-[#505050] mb-10">
                         Write a few sentences about your company.
                       </p>
                       {/* //////////////////////////// */}
@@ -211,9 +207,7 @@ const Profile = () => {
                       {/* BOTON */}
                       <button
                         /* type="submit" */
-                        class="mt-10 inline-block bg-logo w-3/4 rounded 5ec3bf px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#000000] transition duration-150 ease-in-out hover:bg-[#3a8a87] hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.3),0_4px_18px_0_rgba(0,0,0,0.2)]"
-                        // data-te-ripple-init
-                        // data-te-ripple-color="light"
+                        class="mt-10 inline-block bg-logo w-3/4 dark:bg-[#3a8a87] rounded-md px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#000000] transition duration-150 ease-in-out dark:hover:bg-logo hover:bg-[#3a8a87] hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.3),0_4px_18px_0_rgba(0,0,0,0.2)]"
                         onClick={handleSubmit}
                       >
                         Update
@@ -232,61 +226,3 @@ const Profile = () => {
 };
 
 export default Profile;
-
-//! Modificar checkbox
-{
-  /* <div class="flex h-6 items-center">
-<input
-  id="candidates"
-  name="candidates"
-  type="checkbox"
-  class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-/>
-</div> */
-}
-
-//! Agregar una foto
-{
-  /* <div class="col-span-full">
-<label
-  for="cover-photo"
-  class="block text-sm font-medium leading-6 text-gray-900"
->
-  Cover photo
-</label>
-<div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-  <div class="text-center">
-    <svg
-      class="mx-auto h-12 w-12 text-gray-300"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      aria-hidden="true"
-    >
-      <path
-        fill-rule="evenodd"
-        d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
-        clip-rule="evenodd"
-      />
-    </svg>
-    <div class="mt-4 flex text-sm leading-6 text-gray-600">
-      <label
-        for="file-upload"
-        class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
-      >
-        <span>Upload a file</span>
-        <input
-          id="file-upload"
-          name="file-upload"
-          type="file"
-          class="sr-only"
-        />
-      </label>
-      <p class="pl-1">or drag and drop</p>
-    </div>
-    <p class="text-xs leading-5 text-gray-600">
-      PNG, JPG, GIF up to 10MB
-    </p>
-  </div>
-</div>
-</div> */
-}
