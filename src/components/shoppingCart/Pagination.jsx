521eebc9d91710b4b0e11cfdc188312b4e4ca917
UBergesio <ulibergesio@gmail.com>
2023-10-25 00:06:45 -0300

    fix shop

--- src/components/shoppingCart/Pagination.jsx
+++ src/components/shoppingCart/Pagination.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import { useDispatch } from 'react-redux';
 import { setPage } from '../../Redux/actions';
 
-const Pagination = ({ currentPage }) => {
+const Pagination = ({ currentPage, numTemplates, templates}) => {
   const dispatch = useDispatch();
+  if (templates === 0) {
+    dispatch(setPage(1))
+  }
   const handlePageChange = (pageNumber) => {
     dispatch(setPage(pageNumber));
   };
@@ -16,13 +19,11 @@ const Pagination = ({ currentPage }) => {
       >
         Back
       </button>
-      <span className="text-white m-4">
-        Page {currentPage}
-      </span>
+      <span className="text-white m-4">Page {currentPage}</span>
       <button
         className="text-white m-4   inline-block bg-[#202020] w-29 rounded px-2 pb-1.5 pt-2 text-xs font-medium uppercase leading-normal text-white text-opacity-60 shadow-[0_4px_9px_-4px_#000000] transition duration-150 ease-in-out hover:bg-[#303030] hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.3),0_4px_18px_0_rgba(0,0,0,0.2)]"
         onClick={() => handlePageChange(currentPage + 1)}
-        disabled={currentPage === 2}
+        disabled={numTemplates === 0}
       >
         Next
       </button>
