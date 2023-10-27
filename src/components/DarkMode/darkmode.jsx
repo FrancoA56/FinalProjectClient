5e2344246ab8ac2cdea313cf35b4901feb27eb7c
Tomas Iglesias <92072358+zTom1909@users.noreply.github.com>
2023-10-27 19:11:32 -0300

    Fixed themes button changing the height of the navBar

--- src/components/DarkMode/darkmode.jsx
+++ src/components/DarkMode/darkmode.jsx
@@ -49,7 +49,7 @@ const DarkMode = () => {
       {isDarkMode ? (
         <a
           id="theme-switcher-light"
-          class=" w-auto whitespace-nowrap bg-transparent px-3 py-2 text-sm font-normal text-gray-700 focus:bg-transparent focus:outline-none active:text-zinc-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-gray-400 dark:text-gray-100 dark:hover:bg-transparent"
+          class="h-10 w-auto whitespace-nowrap bg-transparent px-3 py-2 text-sm font-normal text-gray-700 focus:bg-transparent focus:outline-none active:text-zinc-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-gray-400 dark:text-gray-100 dark:hover:bg-transparent"
           href="#"
           data-theme="light"
           onClick={() => onThemeSwitcherItemClick("light")}
